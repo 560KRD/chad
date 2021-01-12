a269eeed76e9b4149a701cfeef44dc126379bb64
Ali A <alide12332@gmail.com>
2021-01-12 21:43:12 +0000

    formatting

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 require('dotenv').config();
